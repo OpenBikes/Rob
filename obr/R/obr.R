bbd4939ced866df695fd46f36c7c2f11aa28a7ad
Axel Bellec <axel.bellec@outlook.fr>
2016-11-06 16:16:46 +0100

    write package documentation

--- obr/R/obr.R
+++ obr/R/obr.R
@@ -1,7 +1,16 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
 library('rjson')
 library('jsonlite')
 library('httr')
-
 
 BASE_URL <- "http://api.openbikes.co"
 
@@ -14,12 +23,12 @@ snake_case <- function(text) {
 
 get_latest_geojson <- function(city=NULL) {
   # Return the latest geojson file of a city.
-  
+
   if (is.null(city)) {
     stop('No city name supplied.')
   }
   url <- sprintf("%s/geojson/%s", BASE_URL, city)
-  
+
   response <- jsonlite::fromJSON(url, flatten=TRUE)
   colnames(response$features) <- snake_case(colnames(response$features))
   return (response)
@@ -27,57 +36,57 @@ get_latest_geojson <- function(city=NULL) {
 
 get_countries <- function(provider=NULL) {
   # Return the list of countries.
-  
+
   url <- sprintf("%s/countries", BASE_URL)
-  
+
   if (!is.null(provider)) {
     url <- sprintf("%s?provider=%s", url, provider)
   }
-  
-  response <- GET(url)
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return (unlist(jsonlite::fromJSON(content(response, "text"), simplifyVector = FALSE)))
+
+  return (unlist(jsonlite::fromJSON(httr::content(response, "text"), simplifyVector = FALSE)))
 }
 
 get_providers <- function(country=NULL) {
   # Return the list of providers.
-  
+
   url <- sprintf("%s/providers", BASE_URL)
-  
+
   if (!is.null(country)) {
     url <- sprintf("%s?country=%s", url, country)
   }
-  
-  response <- GET(url)
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return (unlist(jsonlite::fromJSON(content(response, "text"), simplifyVector = FALSE)))
+
+  return (unlist(jsonlite::fromJSON(httr::content(response, "text"), simplifyVector = FALSE)))
 }
 
 get_metrics <- function() {
   # Returns latest metrics.
-  
+
   url <- sprintf("%s/metrics", BASE_URL)
-  
-  response <- GET(url)
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return (jsonlite::fromJSON(content(response, "text"), simplifyVector = FALSE))
+
+  return (jsonlite::fromJSON(httr::content(response, "text"), simplifyVector = FALSE))
 }
 
 get_cities <- function(slug=NULL, country=NULL, provider=NULL, predictable=NULL, active=NULL) {
   # Return the list of cities.
-  
+
   url <- sprintf("%s/cities", BASE_URL)
   print(as.list(match.call()))
-  
+
   if (!is.null(slug)) {
     url <- paste(url, "?slug=", slug, sep='')
   }
@@ -93,117 +102,105 @@ get_cities <- function(slug=NULL, country=NULL, provider=NULL, predictable=NULL,
   if (!is.null(active)) {
     url <- paste(url, "?active=", active, sep='')
   }
-  
-  response <- GET(url)
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return (jsonlite::fromJSON(content(response, "text"), flatten=TRUE))
+
+  return (jsonlite::fromJSON(httr::content(response, "text"), flatten=TRUE))
 }
 
 get_updates <- function(city_slug=NULL) {
   # Return the list of latest updates for each city.
-  
+
   url <- sprintf("%s/updates", BASE_URL)
-  
+
   if (!is.null(city_slug)) {
     url <- sprintf("%s?city_slug=", city_slug)
   }
-  
-  response <- GET(url)
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return (jsonlite::fromJSON(content(response, "text"), flatten=TRUE))
+
+  return (jsonlite::fromJSON(httr::content(response, "text"), flatten=TRUE))
 }
 
 get_stations <- function(slug=NULL, city_slug=NULL) {
   # Return the list of stations.
-  
+
   url <- sprintf("%s/stations", BASE_URL)
-  
+
   if (!is.null(slug)) {
     url <- paste(url, "?slug=", slug, sep='')
   }
   if (!is.null(city_slug)) {
     url <- paste(url, "?city_slug=", city_slug, sep='')
   }
-  
-  response <- GET(url)
-  
-  if (http_type(response) != "application/json") {
-    stop("API did not return json", call. = FALSE)
-  }
-  
-  return (jsonlite::fromJSON(content(response, "text"), flatten=TRUE))
-}
 
-get_forecast <- function(city_slug=NULL, station_slug=NULL, kind=NULL, moment=NULL) {
-  # Return a forecast for a station at a given time.
-  
-  if (is.null(city_slug) | is.null(station_slug) | is.null(kind) | is.null(moment)) {
-    stop("city_slug, station_slug, kind and moment are required.", call. = FALSE)
-  }
-  
-  # FIX: POST JSON data
-  url <- sprintf("%s/forecast", BASE_URL)
-  
-  body <- jsonlite::toJSON(list(city_slug=city_slug, station_slug=station_slug, kind=kind, moment=moment))
-  print(typeof(body))
-  
-  response <- POST(url, body=body, encode="json")
-  print(response)
-  if (http_type(response) != "application/json") {
+  response <- httr::GET(url)
+
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return (jsonlite::fromJSON(content(response, "text"), flatten=TRUE)) 
+
+  return (jsonlite::fromJSON(httr::content(response, "text"), flatten=TRUE))
 }
 
 get_closest_city <- function(latitude=NULL, longitude=NULL) {
   # Return the closest city for a given latitude and longitude.
-  
+
   if (is.null(latitude) | is.null(longitude)) {
     stop("latitude and longitude are required.", call. = FALSE)
   }
   url <- sprintf("%s/closest_city/%s/%s", BASE_URL, latitude, longitude)
-  
-  response <- GET(url)
-  
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return(jsonlite::fromJSON(content(response, "text"), flatten=TRUE))
+
+  return(jsonlite::fromJSON(httr::content(response, "text"), flatten=TRUE))
 }
 
 get_closest_station <- function(latitude=NULL, longitude=NULL) {
   # Return the closest station for a given latitude and longitude.
-  
+
   if (is.null(latitude) | is.null(longitude)) {
     stop("latitude and longitude are required.", call. = FALSE)
   }
   url <- sprintf("%s/closest_station/%s/%s", BASE_URL, latitude, longitude)
-  
-  response <- GET(url)
-  
-  if (http_type(response) != "application/json") {
+
+  response <- httr::GET(url)
+
+  if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
-  return(jsonlite::fromJSON(content(response, "text"), flatten=TRUE))
+
+  return(jsonlite::fromJSON(httr::content(response, "text"), flatten=TRUE))
 }
 
-# geojson <- get_latest_geojson(city="toulouse")
-# countries <- get_countries(provider="keolis")
-# providers <- get_providers(country="France")
-# metrics <- get_metrics()
-# updates <- get_updates()
-# cities <- get_cities(country="France")
-# stations <- get_stations(city_slug="toulouse")
-# closest_city <- get_closest_city(43.556982, 1.466525)
-# closest_station <- get_closest_station(43.556982, 1.466525)
-# forecast <- get_forecast(city_slug='toulouse', station_slug="00003-pomme", kind="bikes", moment="1477398413.144025")
+get_forecast <- function(city_slug=NULL, station_slug=NULL, kind=NULL, moment=NULL) {
+  # Return a forecast for a station at a given time.
 
+  if (is.null(city_slug) | is.null(station_slug) | is.null(kind) | is.null(moment)) {
+    stop("city_slug, station_slug, kind and moment are required.", call. = FALSE)
+  }
+
+  # FIX: POST JSON data
+  url <- sprintf("%s/forecast", BASE_URL)
+
+  body <- jsonlite::toJSON(list(city_slug=city_slug, station_slug=station_slug, kind=kind, moment=moment))
+  print(body)
+
+  response <- httr::POST(url, body=body, encode="json")
+  print(response)
+  if (httr::http_type(response) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  return (jsonlite::fromJSON(httr::content(response, "text"), flatten=TRUE))
+}
